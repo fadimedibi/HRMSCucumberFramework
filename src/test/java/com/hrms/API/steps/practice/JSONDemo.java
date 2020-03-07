73bd49242fd3525c4ddf492293c2c71c6338c71c
fadimedibi <fadimedibi@gmail.com>
2020-03-07 13:48:24 -0600

    JSON file changes

--- src/test/java/com/hrms/API/steps/practice/JSONDemo.java
+++ src/test/java/com/hrms/API/steps/practice/JSONDemo.java
@@ -1,11 +1,11 @@
-package com.hrms.API.steps.practice;
-
-public class JSONDemo {
-public static void main(String[] args) {
-	
-String storeText=readJson(STORE_JSON);
-
-	
-	
-}
-}
+//package com.hrms.API.steps.practice;
+//
+//public class JSONDemo {
+//public static void main(String[] args) {
+//	
+//String storeText=readJson(STORE_JSON);
+//
+//	
+//	
+//}
+//}
